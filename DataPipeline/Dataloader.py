4aa1ea4a1cf1373f9e7c9018f99a8361283b590c
UnbreakablePACHES <wy244950123@gmail.com>
2025-06-17 21:49:49 +0900

    initial LR+LSTM

--- DataPipeline/Dataloader.py
+++ DataPipeline/Dataloader.py
@@ -1,23 +1,24 @@
 import torch
 from torch.utils.data import Dataset
 
-class Dataloader(Dataset):
+class PortfolioDataset(Dataset):
     def __init__(self, features_df, labels_df, num_assets):
         """
-        参数:
+        parameter:
         - features_df: shape = (num_samples, num_assets * num_features)
         - labels_df: shape = (num_samples, num_assets)
-        - num_assets: ETF 数量（如 8）
+        - num_assets: Number of assets
         """
         num_features = features_df.shape[1] // num_assets
 
-        self.X = features_df.values.reshape(-1, num_assets, num_features)  # shape: (N, 8, 7)
-        self.y = labels_df.values  # shape: (N, 8)
+        self.X = features_df.values.reshape(-1, num_assets, num_features) 
+        # shape: (T = Time steps, N = Number of assets, F = Number of features)
+        self.y = labels_df.values  # shape: (T, N)
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, idx):
-        x = torch.tensor(self.X[idx], dtype=torch.float32)  # shape: (8, 7)
-        y = torch.tensor(self.y[idx], dtype=torch.float32)  # shape: (8,)
+        x = torch.tensor(self.X[idx], dtype=torch.float32)  # shape: (T, F)
+        y = torch.tensor(self.y[idx], dtype=torch.float32)  # shape: (F,)
         return x, y
